6ca4e06b51570b8b97e5baf01e7d5847c571bbf8
eugencius <viniciuseugeniovhe@gmail.com>
2023-10-02 15:25:22 -0300

    Build: Enhancing the model of the product

--- apps/products/models.py
+++ apps/products/models.py
@@ -4,11 +4,10 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Product(models.Model):
-    stock = models.PositiveIntegerField(default=1)
-    name = models.CharField(verbose_name=_("Name"), max_length=150)
-    slug = models.SlugField()
+    name = models.CharField(verbose_name=_("Name"), max_length=64)
+    slug = models.SlugField(blank=True)
     short_description = models.CharField(
-        verbose_name=_("Short description"), max_length=255
+        verbose_name=_("Short description"), max_length=128
     )
     long_description = models.TextField(verbose_name=_("Long description"))
     price = models.FloatField(verbose_name=_("Price"))
@@ -16,6 +15,8 @@ class Product(models.Model):
         verbose_name=_("Promotional price"), default=0
     )
     cover = models.ImageField(verbose_name=_("Cover"), upload_to="media/images/")
+    stock = models.PositiveIntegerField(verbose_name=_("Stock"))
+    is_published = models.BooleanField(verbose_name=_("Is published"), default=True)
     created_at = models.DateTimeField(verbose_name=_("Created at"), auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name=_("Updated at"), auto_now=True)
 
