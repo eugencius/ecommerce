48bbd7a0e6756ddebc7630084b62e8f847ce29b6
eugencius <viniciuseugeniovhe@gmail.com>
2023-10-03 14:24:09 -0300

    Feat: Implement a modified class for the Product Admin

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "slug",
+        "price",
+        "promotional_price",
+        "stock",
+        "updated_at",
+    )
+
+    list_display_links = (
+        "id",
+        "name",
+    )
+
+    list_per_page = 25
+
+    prepopulated_fields = {
+        "slug": ("name",),
+    }
